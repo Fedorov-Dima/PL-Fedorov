04ea38330f31174d4f84cbfabca3bb121d31c263
Fedorov-Dima <fedorov.2006@yandex.ru>
2025-12-01 15:06:56 +0300

    ПРЗ 12

--- myfirstproject/myapp/views.py
+++ myfirstproject/myapp/views.py
@@ -10,3 +10,12 @@ def about(request):
 
 def contact(request):
     return HttpResponse("<h2>Контакты</h2>")
+
+def products(request, productid):
+    category = request.GET.get("cat", '')
+    output = "<h2>Product №{0} Category: {1}</h2>".format(productid, category)
+    return HttpResponse(output)
+
+def users(request, id, name):
+    output = f"<h2>Пользователь</h2><h3>id: {id} имя: {name}</h3>"
+    return HttpResponse(output)
--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
 from myapp import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
-    path('contact', views.contact)
+    re_path(r'^products/(?P<productid>\d+)/', views.products),
+    re_path(r'^users/(?P<id>\d+)/(?P<name>\D+)/', views.users),
+    re_path(r'^about/contact/', views.contact),
+    re_path(r'^about', views.about),
+    path('', views.index)
 ]
